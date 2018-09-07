d08d0b1357abfb436c69bf7a7a575908e86dc117
jcfu <jcfu@manzanita-236-100.arc.nasa.gov>
2018-09-07 09:15:26 -0700

    ok

--- Test Suites/smoke_test.ts
+++ Test Suites/smoke_test.ts
@@ -4,7 +4,7 @@
    <name>smoke_test</name>
    <tag></tag>
    <isRerun>false</isRerun>
-   <lastRun>2018-09-05T20:29:37</lastRun>
+   <lastRun>2018-09-06T21:29:35</lastRun>
    <mailRecipient></mailRecipient>
    <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>10</pageLoadTimeout>
